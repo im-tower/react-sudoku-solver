be1f70236a94b3ef248d5e981e45a6e709470220
Fernando Amthauer <fernando.amthauer@coolebra.com>
2023-11-04 19:50:47 -0300

    Clear default board

--- src/components/GameUI.jsx
+++ src/components/GameUI.jsx
@@ -6,7 +6,7 @@ import { useEffect } from "react";
 export default function GameUI(){
 
     const [selectedBoard, setBoard] = useState([
-        ["1", "1", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", "", ""],
         ["", "", "", "", "", "", "", "", ""],
         ["", "", "", "", "", "", "", "", ""],
         ["", "", "", "", "", "", "", "", ""],
