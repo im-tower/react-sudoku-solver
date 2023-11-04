b2a4cd16021d7538904b69da32a1c70711ffbb7c
Fernando Amthauer <fernando.amthauer@coolebra.com>
2023-11-04 16:58:33 -0300

    Gameboard arrow keys navigation

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 
 export default function GameBoard({board, setBoard}){
 
-    const [selectedSquare, setSelectedSquare] = useState({x: 0, y: 0});
+    const [selectedSquare, setSelectedSquare] = useState({x: -1, y: -1});
     const [numbers_grid_visible, setNumbersGridVisible] = useState(false);
     const [numbers_grid_position, setNumbersGridPosition] = useState({
         x: 0,
@@ -35,11 +35,41 @@ export default function GameBoard({board, setBoard}){
         const newBoard = copyBoard(board);
         newBoard[selectedSquare.y][selectedSquare.x] = value;
         setBoard(newBoard);
-        setSelectedSquare({x: 0, y: 0});
+        setSelectedSquare({x: -1, y: -1});
         setNumbersGridVisible(false);
     }
+    const onKeyUp = ({key}) => {
+        if(key === "Escape"){
+            setSelectedSquare({x: -1, y: -1});
+            setNumbersGridVisible(false);
+        }else if(key === "ArrowUp"){
+            const isTop = selectedSquare.y - 1 < 0;
+            const newY = isTop ? 8 : selectedSquare.y - 1;
+            setSelectedSquare({x: selectedSquare.x, y: newY});
+            const newYPosition = isTop ? numbers_grid_position.y+50*8 : numbers_grid_position.y - 50;
+            setNumbersGridPosition({...numbers_grid_position, y: newYPosition});
+        }else if(key === "ArrowDown"){
+            const isBottom = selectedSquare.y + 1 > 8;
+            const newY = isBottom ? 0 : selectedSquare.y + 1;
+            setSelectedSquare({x: selectedSquare.x, y: newY});
+            const newYPosition = isBottom ? numbers_grid_position.y-50*8 : numbers_grid_position.y + 50;
+            setNumbersGridPosition({...numbers_grid_position, y: newYPosition});
+        }else if(key === "ArrowLeft"){
+            const isLeft = selectedSquare.x - 1 < 0;
+            const newX = isLeft ? 8 : selectedSquare.x - 1;
+            setSelectedSquare({x: newX, y: selectedSquare.y});
+            const newXPosition = isLeft ? numbers_grid_position.x+50*8 : numbers_grid_position.x - 50;
+            setNumbersGridPosition({...numbers_grid_position, x: newXPosition});
+        }else if(key === "ArrowRight"){
+            const isRight = selectedSquare.x + 1 > 8;
+            const newX = isRight ? 0 : selectedSquare.x + 1;
+            setSelectedSquare({x: newX, y: selectedSquare.y});
+            const newXPosition = isRight ? numbers_grid_position.x-50*8 : numbers_grid_position.x + 50;
+            setNumbersGridPosition({...numbers_grid_position, x: newXPosition});
+        }
+    }
     return(
-        <div className="board-container">
+        <div className="board-container" onKeyUp={onKeyUp}>
             {
                 Array.from(Array(9).keys()).map((i) => 
                     <div key={i} className="board-row">
