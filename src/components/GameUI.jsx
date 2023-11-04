9b24f9784663b285ebecd64e561f185b54a2ac5c
Fernando Amthauer <fernando.amthauer@coolebra.com>
2023-11-04 19:46:34 -0300

    Solutions founded visualizer

--- src/components/GameUI.jsx
+++ src/components/GameUI.jsx
@@ -1,23 +1,12 @@
 import GameBoard from "./GameBoard";
 import { useState } from "react";
-import { copyBoard, solveBoard } from "../features/solver";
+import { copyBoard, equals, solveBoard } from "../features/solver";
+import { useEffect } from "react";
 
 export default function GameUI(){
 
-    /*const [board, setBoard] = useState([
-        ["", 9, "", 7, "", "", "", "", ""],
-        ["", "", 2, "", "", "", 3, "", ""],
-        [4, "", "", "", 5, 9, "", 1, ""],
-        ["", "", "", 4, "", "", "", 7, ""],
-        ["", 3, "", "", 7, 6, 1, "", ""],
-        [6, "", "", 8, "", "", "", "", ""],
-        [9, "", "", "", 6, 1, "", 5, ""],
-        ["", "", "", "", "", 8, "", "", ""],
-        ["", 4, "", "", "", "", "", "", 9]
-    ]);*/
-
-    const [board, setBoard] = useState([
-        ["", "", "", "", "", "", "", "", ""],
+    const [selectedBoard, setBoard] = useState([
+        ["1", "1", "", "", "", "", "", "", ""],
         ["", "", "", "", "", "", "", "", ""],
         ["", "", "", "", "", "", "", "", ""],
         ["", "", "", "", "", "", "", "", ""],
@@ -28,15 +17,56 @@ export default function GameUI(){
         ["", "", "", "", "", "", "", "", ""]
     ]);
 
+    const [selectedBoardIndex, setSelectedBoardIndex] = useState(0);
+    const [boards, setBoards] = useState([]);
+    const [nonSoluble, setNonSoluble] = useState(false);
+
     const onSolve = () => {
-        const newBoard = solveBoard(copyBoard(board));
-        setBoard(newBoard);
+        console.log("Solving")
+        const copyOfBoard = copyBoard(selectedBoard);
+        console.log(copyOfBoard)
+        const tries = 10;
+        const maxTime = 0.06;
+        let solutions = [];
+        const startTime = Date.now();
+        for(let try_index = 0; try_index < tries; ++try_index){
+            const solution = solveBoard(copyBoard(copyOfBoard));
+            if(solution !== null) solutions.push(solution);
+            if(Date.now() - startTime > maxTime * 1000) break;
+        }
+        if(solutions.length === 0){
+            solutions.push(copyOfBoard);
+            setNonSoluble(true);
+            return;
+        }else{
+            setNonSoluble(false);
+        }
+        solutions = solutions.filter((solution, index) => solutions.find((other) => !equals(solution, other)) !== undefined || index === 0);
+        setBoards(solutions);
+        setSelectedBoardIndex(0);
     }
+
+    useEffect(() => {
+        if(boards.length === 0) return;
+        setBoard(boards[selectedBoardIndex]);
+    }, [selectedBoardIndex, boards]);
 
     return(
         <div className="game-ui-container">
-            <GameBoard board={board} setBoard={setBoard} />
-            <button className="solve-btn" onClick={onSolve}>Solve</button>
+            <GameBoard board={selectedBoard} setBoard={setBoard} />
+            <div className="game-ui-buttons">
+                <button
+                    onClick={() => setSelectedBoardIndex(selectedBoardIndex-1)}
+                    style={{visibility: (selectedBoardIndex === 0 ? 'hidden' : 'visible')}}
+                >{'<'}</button>
+                <button className="solve-btn" onClick={onSolve}>Solve</button>
+                <button
+                    onClick={() => setSelectedBoardIndex(selectedBoardIndex+1)}
+                    style={{visibility: (selectedBoardIndex === boards.length - 1 || boards.length === 0 ? 'hidden' : 'visible')}}
+                >{'>'}</button>
+            </div>
+            <p className="non-soluble" style={{visibility: (nonSoluble ? 'visible' : 'hidden')}}>This board cannot be solved</p>
+            <p className="solutions-counter" style={{visibility: (boards.length > 0 ? 'visible' : 'hidden')}}>{boards.length} solutions founded.</p>
         </div>
     )
 }
